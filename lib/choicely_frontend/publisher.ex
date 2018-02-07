443147f4da7202990abc4f753cf88d9133c28026
Roman Boiko <leanersoftware@gmail.com>
2018-02-07 10:50:09 +0200

    feat(publisher): batch processing

---
+++ lib/choicely_frontend/publisher.ex
@@ -0,0 +1,17 @@
+defmodule ChoicelyFrontend.Publisher do
+  alias ChoicelyFrontend.RequestSchema, as: Schema
+  alias ChoicelyFrontend.GooglePubSub, as: PubSub
+
+  def publish(%{"_json" => list}) when is_list(list), do: Enum.map(list, &publish_item/1)
+  def publish(params), do: publish_item(params)
+  defp publish_item(item) do
+    case Schema.validate(item) do
+      :ok ->
+        PubSub.publish("choicely-votefront", "vote", item)
+        :published
+      {:error, errors} -> %{"errors" => encode(errors), "item" => item}
+    end
+  end
+
+  defp encode(errors), do: Enum.map(errors, fn {key, val} -> %{key => val} end)
+end
--- lib/choicely_frontend_web/controllers/message_controller.ex
+++ lib/choicely_frontend_web/controllers/message_controller.ex
@@ -1,22 +1,9 @@
 defmodule ChoicelyFrontendWeb.MessageController do
   use ChoicelyFrontendWeb, :controller
-  alias ChoicelyFrontend.RequestSchema, as: Schema
-  alias ChoicelyFrontend.GooglePubSub, as: PubSub
+  alias ChoicelyFrontend.Publisher
 
   def post(conn, _) do
-    params = conn.body_params
-    data =
-      case Schema.validate(params) do
-        :ok ->
-          PubSub.publish("choicely-votefront", "vote", params)
-          %{"success" => true}
-        {:error, errors} -> %{"errors" => encode(errors), "request" => params}
-      end
+    data = Publisher.publish(conn.body_params)
     render conn, "index.json", data: data
-  end
-
-  defp encode(errors) do
-    errors
-    |> Enum.map(fn {key, val} -> %{key => val} end)
   end
 end
