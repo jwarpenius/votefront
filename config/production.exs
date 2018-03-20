545b269144f780a530e17a7058f406f6c132c407
Jari Warpenius <jari@warpenius.fi>
2018-03-20 22:21:26 +0200

    config file syntax fix

--- config/production.exs
+++ config/production.exs
@@ -16,7 +16,7 @@ use Mix.Config
 config :choicely_votefront, ChoicelyVotefront.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   root: ".",
   version: Application.spec(:choicely_votefront, :vsn)
