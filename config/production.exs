07b37f50ad4377632e971920c69d1b8018fa2858
Jari Warpenius <jari@warpenius.fi>
2018-03-20 22:20:29 +0200

    release spesific configs

--- config/production.exs
+++ config/production.exs
@@ -17,6 +17,9 @@ config :choicely_votefront, ChoicelyVotefront.Endpoint,
   load_from_system_env: true,
   url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
+  server: true,
+  root: ".",
+  version: Application.spec(:choicely_votefront, :vsn)
 
 # Do not print debug messages in production
 config :logger, level: :info
